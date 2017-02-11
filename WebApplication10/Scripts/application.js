00bd74805cda19e40b7fcecfa3d973da14eb257d
Josip Digital <info@digriot.com>
2017-02-11 16:41:29 +0100

    Add project files.

---
+++ WebApplication10/Scripts/application.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('app', ['ngRoute']);
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/services', {
+        templateUrl: 'services.html'
+    })
+    $routeProvider.when('/about', {
+        templateUrl: 'about.html'
+        
+    })
+    $routeProvider.when('/products', {
+        templateUrl: 'products.html'
+    })
+    $routeProvider.when('/', {
+        template: 'Main page'
+    })
+}]);
